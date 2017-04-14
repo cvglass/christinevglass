2106119fad595f54f84f311b9ad129aebc46b3ba
cvglass <christinevglass@gmail.com>
2017-04-13 21:31:18 -0500

    styling

--- app/components/about.js
+++ app/components/about.js
@@ -3,13 +3,17 @@ import React from 'react';
 const About = () => {
   return (
     <div>
-      <p>My elementary-school teachers taught me well. Allow me to introduce myself via the 5 W's (and a stray H)</p>
-      <p>WHO: Christine Glass. I'm a fullstack web developer looking to combine my nonprofit experience with technology.</p>
-      <p>WHAT: Node, Express, Sequelize, React, Redux, React Native</p>
-      <p>WHERE: I originally hail from a suburb outside of Richmond, VA. About six years ago, I moved to Chicago to pursue an advanced degree in Sociology, hoping to become a professor. I ultimately decided I wanted to work at a more grassroots level, but I've since made Chicago my home. I've lived in many parts of the city, but have settled in the Ravenswood neighborhood.</p>
-      <p>WHEN: Now?</p>
-      <p>WHY: I've spend the bulk of my employment history in career services. While helping people find their dream jobs, I did some soul searching to figure out what it was that I wanted to do. I had learned basic HTML/CSS when I was younger, but it fell off at some point. The spark came back when I was working with CNC jobseekers. I had to establish a baseline knowledge of CNC, so they trained me in how to program the machine. It was an immensely satisfying experience to write the code from scratch, have the machine run, and then have a finished product as proof that I created something from nothing. Remembering my previous interest in HTML/CSS when I was younger, I started looking for resources online. Eventually, I discovered Fullstack Academy's evening bootcamp prep course. I fell in love with it and decided to enroll is Fullstack full-time. </p>
-      <p>HOW: </p>
+      <div className="col-md-2" />
+      <div className="col-md-8">
+        <p>My elementary-school teachers taught me well, so allow me to introduce myself via the 5 W's (and a stray H)</p>
+        <p><span className="about-question">who: </span> Christine Glass. I'm a fullstack web developer looking to combine my nonprofit experience with technology.</p>
+        <p><span className="about-question">what: </span> Node, Express, Sequelize, React, Redux, React Native</p>
+        <p><span className="about-question">where: </span> I originally hail from a suburb outside of Richmond, VA. About six years ago, I moved to Chicago to pursue an advanced degree in Sociology, hoping to become a professor. I ultimately decided I wanted to work at a more grassroots level, but I've since made Chicago my home. I've lived in many parts of the city, but have settled in the Ravenswood neighborhood.</p>
+        <p><span className="about-question">when: </span> Whenever I get the chance.</p>
+        <p><span className="about-question">why: </span> I've spend the bulk of my employment history in career services. While helping people find their dream jobs, I did some soul searching to figure out what it was that I wanted to do. I had learned basic HTML/CSS when I was younger, but it fell off at some point. The spark came back when I was working with CNC jobseekers. I had to establish a baseline knowledge of CNC, so they trained me in how to program the machine. It was an immensely satisfying experience to write the code from scratch, have the machine run, and then have a finished product as proof that I created something from nothing. Remembering my previous interest in HTML/CSS when I was younger, I started looking for resources online. Eventually, I discovered Fullstack Academy's evening bootcamp prep course. I fell in love with it and decided to enroll is Fullstack full-time. </p>
+        <p><span className="about-question">how: </span> I code with passion, patience, and humility.</p>
+      </div>
+      <div className="col-md-2" />
     </div>
   )
 }
