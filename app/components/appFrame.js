1901e412ee86c2e74eda92481dd900a1a7bee12a
cvglass <christinevglass@gmail.com>
2017-04-18 18:08:25 -0500

    contact links

--- app/components/appFrame.js
+++ app/components/appFrame.js
@@ -9,7 +9,12 @@ const AppFrame = ({ children }) => {
             <li className="header-name"><a href="/">Christine Glass</a></li>
             <li><a href="/about">About</a></li>
             <li><a href="/portfolio">Portfolio</a></li>
-            <li><a href="/">Contact</a></li>
+          </ul>
+          <ul className="nav navbar-nav contact-icons">
+            <li><a href="https://github.com/cvglass"><i className="fa fa-github" aria-hidden="true"></i></a></li>
+            <li><a href="https://www.linkedin.com/in/christineglass/"><i className="fa fa-linkedin-square" aria-hidden="true"></i></a></li>
+            <li><a href="mailto:christinevglass@gmail.com"><i className="fa fa-envelope-square" aria-hidden="true"></i></a></li>
+            <li><a href="https://twitter.com/christinevglass"><i className="fa fa-twitter-square" aria-hidden="true"></i></a></li>
           </ul>
         </div>
       </nav>
