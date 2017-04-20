7f4c8ebecf5bb9eb8f68a581f53a4200afb49c63
cvglass <christinevglass@gmail.com>
2017-04-20 13:03:20 -0500

    removes excess server files

--- app/main.jsx
+++ app/main.jsx
@@ -2,22 +2,18 @@
 import React from 'react'
 import { Router, Route, IndexRedirect, browserHistory } from 'react-router'
 import { render } from 'react-dom'
-import { Provider } from 'react-redux'
 
-import store from './store'
 import About from './components/about';
 import AppFrame from './components/appFrame';
 import Portfolio from './components/portfolio';
 
 render (
-  <Provider store={store}>
     <Router history={browserHistory}>
       <Route path="/" component={AppFrame}>
         <Route path="/about" component={About} />
-        <IndexRedirect to="/about" />
         <Route path="/portfolio" component={Portfolio} />
+        <IndexRedirect to="/about" />
       </Route>
-    </Router>
-  </Provider>,
+    </Router>,
   document.getElementById('main')
 )
